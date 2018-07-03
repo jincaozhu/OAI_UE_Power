6e18c3af19ecd25957b4d5d6b4a52bdad4a3785c
jincaozhu <33697973+jincaozhu@users.noreply.github.com>
2018-07-02 23:25:14 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -155,7 +155,7 @@ else:
     enb1.hardware_type = GLOBALS.NUC_HWTYPE
     enb1.disk_image = GLOBALS.OAI_ENB_IMG
     #enb1.Desire( "rf-radiated" if params.TYPE == "ota" else "rf-controlled", 1 )
-    connectOAI_DS(enb1)
+    connectOAI_DS(enb1,0)
     enb1.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
     enb1_rue1_rf = enb1.addInterface("rue1_rf")
 
@@ -172,9 +172,9 @@ else:
     rue1_enb1_rf = rue1.addInterface("enb1_rf")
 
     # Create the RF link between the Nexus 5 UE and eNodeB
-    rflink2 = request.RFLink("rflink2")
-    rflink2.addInterface(enb1_rue1_rf)
-    rflink2.addInterface(rue1_enb1_rf)
+    #rflink2 = request.RFLink("rflink2")
+    #rflink2.addInterface(enb1_rue1_rf)
+    #rflink2.addInterface(rue1_enb1_rf)
 
     # Add a link connecting the NUC eNB and the OAI EPC node.
     epclink.addNode(enb1)
